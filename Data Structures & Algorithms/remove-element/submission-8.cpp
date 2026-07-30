1d876bb56a6695a517776306d930ea2cda6479f8
Naman Verma <n14082005@gmail.com>
2026-07-30 18:04:42 +0530

    Add: remove-element - submission-8

---
+++ Data Structures & Algorithms/remove-element/submission-8.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+        int n = nums.size();
+        int slow = 0;
+  
+
+        for(int fast = 0;fast < n;fast++){
+
+            if(nums[fast] != val){
+                
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+        
+    }
+};
