f3d5764eba76972334959a5eaa7ee36a23387784
Naman Verma <n14082005@gmail.com>
2026-09-04 23:14:59 +0530

    Add: reverse-linked-list-ii - submission-0

---
+++ Data Structures & Algorithms/reverse-linked-list-ii/submission-0.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+
+        ListNode dummy(0,head);
+
+        ListNode* beforeLeft = &dummy;
+
+        for(int i = 1; i < left;i++){
+            beforeLeft = beforeLeft->next;
+        }
+
+        ListNode* subListStart = beforeLeft->next;
+        ListNode* prev = nullptr;
+        ListNode* curr = subListStart;
+
+        for(int i = 0; i < right - left + 1;i++){
+
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+
+
+
+        }
+
+        beforeLeft->next = curr;
+        subListStart->next = curr;
+
+        return dummy.next;
+
+
+        
+        
+    }
+};
